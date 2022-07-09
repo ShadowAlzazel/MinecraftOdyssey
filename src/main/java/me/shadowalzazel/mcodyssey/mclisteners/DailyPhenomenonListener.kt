f3bd4584d9ba930ff3aa7c4c5e129f7c9cd807ac
ShadowAlzazel <pastenalfredojr@gmail.com>
2022-07-09 11:57:12 -0700

    More phenomenon

--- src/main/java/me/shadowalzazel/mcodyssey/mclisteners/DailyPhenomenonListener.kt
+++ src/main/java/me/shadowalzazel/mcodyssey/mclisteners/DailyPhenomenonListener.kt
@@ -2,29 +2,45 @@ package me.shadowalzazel.mcodyssey.mclisteners
 
 import me.shadowalzazel.mcodyssey.MinecraftOdyssey
 import me.shadowalzazel.mcodyssey.events.*
+import org.bukkit.entity.EntityType
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityDeathEvent
 import org.bukkit.event.world.TimeSkipEvent
 
 object OdysseyDailyPhenomenonListener : Listener {
 
+    var endActivation = false
+
+    // Event Chooser
     @EventHandler
     fun onNewDay(event: TimeSkipEvent) {
-        val currentWorld = event.world
 
-        val worldPhenomenonList = listOf(SolarEclipse(), BreezyDay(), SlimeDay(), Earthquake(), BloodMoon(), BlueMoon())
-        val randomWorldPhenomenon = worldPhenomenonList.random()
-        val rolledRate = (0..100).random()
+        if (endActivation) {
+            val currentWorld = event.world
 
-        //Daily luck
-        val luckConfigAmount = MinecraftOdyssey.instance.config.getInt("player-minimum-for-luck")
-        if (currentWorld.players.size >= luckConfigAmount) {
-            val drawOfFortunes = DrawOfFortunes()
-            drawOfFortunes.phenomenonEffect(currentWorld)
+            val worldPhenomenonList = listOf(SolarEclipse(), BreezyDay(), SlimeDay(), Earthquake(), BloodMoon(), BlueMoon())
+            val randomWorldPhenomenon = worldPhenomenonList.random()
+            val rolledRate = (0..100).random()
+
+            //Daily luck
+            val luckConfigAmount = MinecraftOdyssey.instance.config.getInt("player-minimum-for-luck")
+            if (currentWorld.players.size >= luckConfigAmount) {
+                val drawOfFortunes = DrawOfFortunes()
+                drawOfFortunes.phenomenonEffect(currentWorld)
+            }
+
+            randomWorldPhenomenon.phenomenonActivation(currentWorld, rolledRate)
         }
+    }
 
-        randomWorldPhenomenon.phenomenonActivation(currentWorld, rolledRate)
-
-
+    //Event Activation
+    @EventHandler
+    fun onDefeatEnderDragon(event: EntityDeathEvent) {
+        val dragon = event.entity
+        if (dragon.type == EntityType.ENDER_DRAGON) {
+            println("The end has started at ${event.entity.world.name}")
+            endActivation = true
+        }
     }
 }
